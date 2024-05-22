3bb0aa7534f39e83261129d0b6e0d627063be794
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-05-22 03:42:00 +0000

    V6-2.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed EL-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+
+"use strict"
+
+/**
+ * Check service worker support"
+*/
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-01-Osamah-Hasan/sw.js", {
+    scope: "/ICS2O-Unit-6-01-Osamah-Hasan/",
+  })
+}
+
+function myButtonClicked() {
+  // this is a cookie clicker game
+  // process
+  if (localStorage.clicks) {
+    localStorage.clicks = Number(localStorage.clicks) + 1
+  } else {
+    localStorage.clicks = 1
+  }
+
+  // output
+  document.getElementById('clicks').innerHTML = "You have " + localStorage.clicks + " cookies!"
+}
